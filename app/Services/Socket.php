ba90b3e9767fe678233c89168daba1e92b97e0b5
yozman <yozman@sina.com>
2016-11-18 04:11:41 +0800

    重构示例代码

--- app/Services/Socket.php
+++ app/Services/Socket.php
@@ -13,73 +13,123 @@ use PHPSocketIO\SocketIO;
 class Socket
 {
 
+    /**
+     * 全局长链接
+     *
+     * @var
+     */
+    public static $server;
+
+    /**
+     * 客户端队列
+     *
+     * @var
+     */
+    public static $client;
+
+    /**
+     * 当前套接字
+     *
+     * @var
+     */
+    public $socket;
+
+    /**
+     * 构造函数
+     *
+     * @param int $port
+     */
     public function __construct($port = 2020)
     {
-        $io = new SocketIO($port);
+        if (is_null(static::$server)) {
+            static::$server = new SocketIO($port);
+            static::$server->on('connection', [$this, 'connect']);
+            Worker::runAll();
+        }
+    }
 
-        $io->on('connection', function ($socket) {
-            $socket->addedUser = false;
+    /**
+     * 链接初始化
+     *
+     * @param $socket
+     */
+    public function connect($socket)
+    {
+        // 变量赋值
+        $socket->addedUser = false;
 
-            // when the client emits 'new message', this listens and executes
-            $socket->on('new message', function ($data) use ($socket) {
-                // we tell the client to execute 'new message'
-                $socket->broadcast->emit('new message', array(
-                    'username' => $socket->username,
-                    'message' => $data
-                ));
-            });
+        // 连接客户端
+        $client = new static;
+        $client->socket = $socket;
 
-            // when the client emits 'add user', this listens and executes
-            $socket->on('add user', function ($username) use ($socket) {
-                global $usernames, $numUsers;
-                // we store the username in the socket session for this client
-                $socket->username = $username;
-                // add the client's username to the global list
-                $usernames[$username] = $username;
-                ++$numUsers;
-                $socket->addedUser = true;
-                $socket->emit('login', array(
-                    'numUsers' => $numUsers
-                ));
-                // echo globally (all clients) that a person has connected
-                $socket->broadcast->emit('user joined', array(
-                    'username' => $socket->username,
-                    'numUsers' => $numUsers
-                ));
-            });
+        // 需监听事件
+        $events = ['new message', 'add user', 'typing', 'stop typing', 'disconnect'];
 
-            // when the client emits 'typing', we broadcast it to others
-            $socket->on('typing', function () use ($socket) {
-                $socket->broadcast->emit('typing', array(
-                    'username' => $socket->username
-                ));
-            });
-
-            // when the client emits 'stop typing', we broadcast it to others
-            $socket->on('stop typing', function () use ($socket) {
-                $socket->broadcast->emit('stop typing', array(
-                    'username' => $socket->username
-                ));
-            });
-
-            // when the user disconnects.. perform this
-            $socket->on('disconnect', function () use ($socket) {
-                global $usernames, $numUsers;
-                // remove the username from global usernames list
-                if ($socket->addedUser) {
-                    unset($usernames[$socket->username]);
-                    --$numUsers;
-
-                    // echo globally that this client has left
-                    $socket->broadcast->emit('user left', array(
-                        'username' => $socket->username,
-                        'numUsers' => $numUsers
-                    ));
-                }
-            });
+        // 绑定事件
+        array_walk($events, function ($event) use ($client) {
+            $client->socket->on($event, [$client, 'on' . studly_case($event)]);
         });
 
-        Worker::runAll();
+        // 添加到客户端队列
+        static::$client[] = $client;
+    }
+
+    public function onNewMessage($data)
+    {
+        // we tell the client to execute 'new message'
+        $this->socket->broadcast->emit('new message', [
+            'username' => $this->socket->username,
+            'message' => $data
+        ]);
+    }
+
+    public function onAddUser($username)
+    {
+        global $usernames, $numUsers;
+        // we store the username in the socket session for this socket
+        $this->socket->username = $username;
+        // add the socket's username to the global list
+        $usernames[$username] = $username;
+        ++$numUsers;
+        $this->socket->addedUser = true;
+        $this->socket->emit('login', [
+            'numUsers' => $numUsers
+        ]);
+        // echo globally (all clients) that a person has connected
+        $this->socket->broadcast->emit('user joined', [
+            'username' => $this->socket->username,
+            'numUsers' => $numUsers
+        ]);
+    }
+
+    public function onTyping()
+    {
+        $this->socket->broadcast->emit('typing', [
+            'username' => $this->socket->username
+        ]);
+    }
+
+    public function onStopTyping()
+    {
+        $this->socket->broadcast->emit('stop typing', [
+            'username' => $this->socket->username
+        ]);
+    }
+
+    public function onDisconnect()
+    {
+        global $usernames, $numUsers;
+        // remove the username from global usernames list
+        if ($this->socket->addedUser) {
+            unset($usernames[$this->socket->username]);
+            --$numUsers;
+
+            // echo globally that this socket has left
+            $this->socket->broadcast->emit('user left', [
+                'username' => $this->socket->username,
+                'numUsers' => $numUsers
+            ]);
+        }
     }
 
 }
