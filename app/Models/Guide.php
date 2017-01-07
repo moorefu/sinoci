f76246039b9718730b35f6eaf17aa6a7e849cfd4
yozman <yozman@sina.com>
2017-01-07 12:01:19 +0800

    优化欢迎页

--- app/Models/Guide.php
+++ app/Models/Guide.php
@@ -26,10 +26,11 @@ class Guide extends Model
         // 修正信息
         return strtr($this->welcome(), [
             ' to CodeIgniter!' => '',
+            '40px' => '40px 10px',
             'application/controllers/Welcome.php' => 'controllers/' . APP_ENV . '/Welcome.php',
             'application/views/welcome_message.php' => 'app/Models/Guide.php',
             'CodeIgniter' => '<a href="//github.com/sinoci/sinoci" target="_blank"><strong>sinoci</strong></a>',
-            'container"' => 'container" style="width:600px;margin:10px auto"',
+            'container"' => 'container" style="max-width:600px;margin:10px auto"',
             'user_guide/' => '//github.com/sinoci/sinoci/wiki" target="_blank',
             'Welcome to CodeIgniter<' => 'S I N O C I<',
         ]);
