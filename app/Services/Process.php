76f99278164633ad19872b316af265b10027e007
yozman <yozman@sina.com>
2017-01-07 08:58:58 +0800

    优化流程传参

--- app/Services/Process.php
+++ app/Services/Process.php
@@ -25,12 +25,25 @@ class Process extends Promise
 
     public function next($resolve = null)
     {
+        $resolve = $this->parse($resolve);
         return $this->then($resolve);
     }
 
     public function fail($reject = null)
     {
+        $reject = $this->parse($reject);
         return $this->otherwise($reject);
+    }
+
+    public function parse($input)
+    {
+        if (is_callable($input)) {
+            return $input;
+        }
+
+        list($model, $func) = explode('@', $input);
+
+        return [app($model), $func];
     }
 
 }
