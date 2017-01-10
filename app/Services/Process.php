2c119ebef1b1777a1b5c63cd2566f440f8b9144b
yozman <yozman@sina.com>
2017-01-10 15:23:08 +0800

    修复 Process

--- app/Services/Process.php
+++ app/Services/Process.php
@@ -3,21 +3,28 @@
 namespace App\Services;
 
 use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\Promise\PromiseInterface;
 
 /**
  * 框架组件 - 执行流程
  *
  * @package App\Services
  */
-class Process extends Promise
+class Process implements PromiseInterface
 {
 
     private static $instance;
+    private $promise;
+
+    private function __construct($promise)
+    {
+        $this->promise = $promise;
+    }
 
     public static function instance()
     {
         if (is_null(self::$instance)) {
-            self::$instance = new static;
+            self::$instance = new static(new Promise);
         }
 
         return self::$instance;
@@ -35,7 +42,7 @@ class Process extends Promise
         return $this->otherwise($reject);
     }
 
-    public function parse($input)
+    private function parse($input)
     {
         if (is_callable($input)) {
             return $input;
@@ -44,6 +51,41 @@ class Process extends Promise
         list($model, $func) = explode('@', $input);
 
         return [app($model), $func];
+    }
+
+    public function then(callable $onFulfilled = null, callable $onRejected = null)
+    {
+        return new static($this->promise->then($onFulfilled, $onRejected));
+    }
+
+    public function otherwise(callable $onRejected)
+    {
+        return new static($this->promise->otherwise($onRejected));
+    }
+
+    public function getState()
+    {
+        return $this->promise->getState();
+    }
+
+    public function resolve($value)
+    {
+        return $this->promise->resolve($value);
+    }
+
+    public function reject($reason)
+    {
+        return $this->promise->reject($value);
+    }
+
+    public function cancel()
+    {
+        return $this->promise->cancel();
+    }
+
+    public function wait($unwrap = true)
+    {
+        return $this->promise->wait($unwrap);
     }
 
 }
