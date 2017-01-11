a668e0766dc58b042fe5f10028d8a8baab0efab8
yozman <yozman@sina.com>
2017-01-11 14:07:31 +0800

    优化代码

--- app/Services/Router.php
+++ app/Services/Router.php
@@ -17,16 +17,14 @@ class Router
         $this->route =& $route;
     }
 
-    public function get($uri, $action)
+    public function get($uri, $action = 'show_404')
     {
-        $this->route[$uri]['GET'] = $this->makeCallable($action);
-        return $this;
+        return $this->makeRoute($uri, $action, 'GET');
     }
 
-    public function post($uri, $action)
+    public function post($uri, $action = 'show_404')
     {
-        $this->route[$uri]['POST'] = $this->makeCallable($action);
-        return $this;
+        return $this->makeRoute($uri, $action, 'POST');
     }
 
     private function makeCallable($callable)
@@ -37,6 +35,34 @@ class Router
         }
 
         return $callable;
+    }
+
+    private function makeAction($uri, $method)
+    {
+        foreach ($uri as $url => $action) {
+            if (is_string($url)) {
+               $this->route[$url][$method] = $action;
+            }
+            else {
+               $this->route[$action][$method] = preg_replace_callback('/(\w+\/)(.+)/', function ($matches) {
+                   return $matches[1] . preg_replace_callback('/\/([a-z])/', function ($matches) {
+                       return strtoupper($matches[1]);
+                   }, $matches[2]);
+               }, $action);
+            }
+        }
+    }
+
+    private function makeRoute($uri, $action = 'show_404', $method = 'GET')
+    {
+        if (is_string($uri)) {
+            $this->route[$uri][$method] = $this->makeCallable($action);
+        }
+        else {
+            $this->makeAction($uri, $method);
+        }
+
+        return $this;
     }
 
 }
