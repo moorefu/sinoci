92b91db008da4734ff6ca9eeebee6a9bf59f8c8b
yozman <yozman@sina.com>
2017-02-14 10:48:19 +0800

    修正错误信息

--- app/Traits/Welcome/Sinoci.php
+++ app/Traits/Welcome/Sinoci.php
@@ -14,7 +14,7 @@ trait Sinoci
             ' to CodeIgniter!' => '',
             '40px' => '40px 10px',
             'application/controllers/Welcome.php' => 'controllers/' . APP_ENV . '/Welcome.php',
-            'application/views/welcome_message.php' => 'app/Models/Guide.php',
+            'application/views/welcome_message.php' => 'app/Models/Site.php',
             'CodeIgniter' => '<a href="//github.com/sinoci/sinoci" target="_blank"><strong>sinoci</strong></a>',
             'container"' => 'container" style="max-width:600px;margin:10px auto"',
             'user_guide/' => '//github.com/sinoci/sinoci/wiki" target="_blank',
