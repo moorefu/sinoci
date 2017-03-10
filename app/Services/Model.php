df5f82434ab81b8adec078e951bf64952f8dbe07
yozman <yozman@sina.com>
2017-03-10 15:14:16 +0800

    添加动态适配调用前缀

--- app/Services/Model.php
+++ app/Services/Model.php
@@ -9,4 +9,21 @@ namespace App\Services;
  */
 class Model
 {
+
+    public function __call($func, $args)
+    {
+        if (starts_with($func, $prefix = 'adapt')) {
+            if (method_exists($this, $before = array_get($args, count($args) - 2))) {
+                $args = call_user_func([$this, $before], $args);
+            }
+            $data = call_user_func_array([$this, substr($func, strlen($prefix))], $args);
+            if (method_exists($this, $after = array_get($args, count($args) - 1))) {
+                $data = call_user_func([$this, $after], $data);
+            }
+            return $data;
+        }
+
+        throw new \Error('Call to undefined method ' . get_called_class() . "::{$func}()");
+    }
+
 }
