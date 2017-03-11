9df6db509534d4ad6f459cbdbac84b4669607ab9
yozman <yozman@sina.com>
2017-03-11 22:25:05 +0800

    优化代码

--- app/Services/Model.php
+++ app/Services/Model.php
@@ -13,11 +13,12 @@ class Model
     public function __call($func, $args)
     {
         if (starts_with($func, $prefix = 'adapt')) {
-            if (method_exists($this, $before = array_get($args, count($args) - 2))) {
+            $func = substr($func, strlen($prefix));
+            if (method_exists($this, $before = "before{$func}")) {
                 $args = call_user_func([$this, $before], $args);
             }
-            $data = call_user_func_array([$this, substr($func, strlen($prefix))], $args);
-            if (method_exists($this, $after = array_get($args, count($args) - 1))) {
+            $data = call_user_func_array([$this, $func], $args);
+            if (method_exists($this, $after = "after{$func}")) {
                 $data = call_user_func([$this, $after], $data);
             }
             return $data;
