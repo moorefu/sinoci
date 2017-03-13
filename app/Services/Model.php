f8147931643fa84b4fec23ab23570b4191b4d552
yozman <yozman@sina.com>
2017-03-13 11:39:31 +0800

    优化适配器实现

--- app/Services/Model.php
+++ app/Services/Model.php
@@ -10,17 +10,34 @@ namespace App\Services;
 class Model
 {
 
+    private $adapt;
+
+    public function adapt($before, $after)
+    {
+        $model = new self;
+        $model->adapt = [
+            'model' => get_called_class(),
+            'before' => $before,
+            'after' => $after
+        ];
+        return $model;
+    }
+
     public function __call($func, $args)
     {
-        if (starts_with($func, $prefix = 'adapt')) {
-            $func = substr($func, strlen($prefix));
-            if (method_exists($this, $before = "before{$func}")) {
-                $args = call_user_func([$this, $before], $args);
+        if ($this->adapt) {
+            $model = new $this->adapt['model'];
+
+            if (method_exists($model, $before = $this->adapt['before'])) {
+                $args = call_user_func([$model, $before], $args);
             }
-            $data = call_user_func_array([$this, $func], $args);
-            if (method_exists($this, $after = "after{$func}")) {
-                $data = call_user_func([$this, $after], $data);
+
+            $data = call_user_func_array([$model, $func], $args);
+
+            if (method_exists($model, $after = $this->adapt['after'])) {
+                $data = call_user_func([$model, $after], $data);
             }
+
             return $data;
         }
 
