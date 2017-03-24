59ee07339665b2a49a7b36655d39d6a718aef63f
yozman <yozman@sina.com>
2017-03-24 22:39:12 +0800

    优化错误异常处理

--- app/Services/Event.php
+++ app/Services/Event.php
@@ -17,8 +17,18 @@ class Event
      */
     public function error()
     {
+        // 忽略框架错误
+        if (str_contains(func_get_arg(2), str_replace('/', DIRECTORY_SEPARATOR, BASEPATH))) {
+            return;
+        }
+
         // 捕捉非框架错误
-        str_contains(func_get_arg(2), str_replace('/', DIRECTORY_SEPARATOR, BASEPATH)) OR call_user_func_array('_error_handler', func_get_args());
+        if (APP_DEBUG) {
+            call_user_func_array('_error_handler', func_get_args());
+        }
+
+        // 显示错误信息
+        show_error(func_get_arg(1));
     }
 
     /**
@@ -29,7 +39,12 @@ class Event
     public function exception()
     {
         // 捕捉异常
-        call_user_func_array('_exception_handler', func_get_args());
+        if (APP_DEBUG) {
+            call_user_func_array('_exception_handler', func_get_args());
+        }
+
+        // 显示错误信息
+        show_error(func_get_arg(0)->getMessage());
     }
 
 }
