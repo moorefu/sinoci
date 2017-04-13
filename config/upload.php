9b7f53c7d81cfdbf670620927ce00ec15c430c45
柚子男 <yozman@sina.com>
2017-04-13 15:08:47 +0800

    修改文件上传配置

--- config/upload.php
+++ config/upload.php
@@ -1,6 +1,7 @@
 <?php
 
 // 配置文件上传
-$config['upload_path'] = APPPATH . 'storage/upload';
 $config['allowed_types'] = 'gif|jpg|png';
-$config['encrypt_name'] = true;
+$config['file_ext_tolower'] = true;
+$config['overwrite'] = true;
+$config['upload_path'] = APPPATH . 'storage/upload';
