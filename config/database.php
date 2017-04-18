5d88e0c7b21080022bd0e9b1ee7a59cf87dd73c3
yozman <yozman@sina.com>
2017-04-19 02:30:51 +0800

    修改数据库默认配置

--- config/database.php
+++ config/database.php
@@ -3,9 +3,9 @@
 // 线上环境数据库设置
 $config['driver'] = 'mysql';
 $config['host'] = env('DB_HOST', 'localhost');
-$config['database'] = env('DB_DATABASE', 'database');
-$config['username'] = env('DB_USERNAME', 'username');
-$config['password'] = env('DB_PASSWORD', 'password');
+$config['database'] = env('DB_DATABASE', 'test');
+$config['username'] = env('DB_USERNAME', 'root');
+$config['password'] = env('DB_PASSWORD', '');
 $config['charset'] = 'utf8';
 $config['collation'] = 'utf8_unicode_ci';
 $config['prefix'] = '';
