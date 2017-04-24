79ee4a0b48ceb0c3ea211a17fc66e352f766b921
柚子男 <yozman@sina.com>
2017-04-24 22:17:37 +0800

    添加注释

--- app/Models/Util.php
+++ app/Models/Util.php
@@ -15,18 +15,22 @@ class Util extends Model implements Logic
     
     public function upload($file, $path = null)
     {
+        // 初始化类库
         app()->upload->initialize(config('upload') + [
             'file_name' => md5_file($_FILES[$file]['tmp_name'])
         ]);
         
+        // 设置子目录
         if (is_string($path)) {
             app()->upload->upload_path = config('upload.upload_path') . $path;
         }
             
+        // 检测目录正确性
         if (noFile($path = app()->upload->upload_path)) {
             mkdir($path) && mkdir($path . 'thumb/');
         }
         
+        // 执行上传操作
         return app()->upload->do_upload($file);
     }
     
